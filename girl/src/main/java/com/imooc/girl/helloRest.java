15ee6b3904ecdca004f553983f4dc30f92aaf8bb
cxm <978797549@qq.com>
2017-07-11 11:03:59 +0800

    ssdsf

---
+++ girl/src/main/java/com/imooc/girl/helloRest.java
@@ -0,0 +1,29 @@
+package com.imooc.girl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class helloRest {
+//    @Value("${cupSize}")
+//    private  String cupSize;
+
+    @Autowired
+    private GirlProperties girlProperties;
+
+//    @GetMapping (value = {"/hello/{id}","/hi"})
+//    public String hello(@PathVariable("id") Integer id){
+//        return girlProperties.getCupSize()+girlProperties.getAge()+id;
+//    }
+
+//    @GetMapping (value = {"/hello","/hi"})
+//    public String hello(@RequestParam(value="id",required=false,defaultValue = "11") Integer myId){
+//        return girlProperties.getCupSize()+girlProperties.getAge()+myId;
+//    }
+
+    @PostMapping (value = {"/hello","/hi"})
+    public String hello(@RequestParam(value="id",required=false,defaultValue = "11") Integer myId){
+        return girlProperties.getCupSize()+girlProperties.getAge()+myId;
+    }
+}
---
+++ girl/src/main/java/com/imooc/girl/mapper/GirlMapper.java
@@ -0,0 +1,9 @@
+package com.imooc.girl.mapper;
+
+/**
+ * Created by chen on 2017/7/10.
+ */
+
+public interface GirlMapper {
+
+}
